604cdc9e857148d57811908f9c20d909f00d0654
Karan Chaudhary <karan.chaudhary@aitc.ai>
2024-01-04 15:04:56 +0545

    update changes animation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,6 +8,13 @@ export default function About() {
       <TransistionAnimation title={"about"} />
 
       <LandingSection />
+      {/* <div
+        className=" bg-transparent h-[20vh] mt-[-19vh] z-[10] "
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(243,241,244,1) 0%, rgba(35,54,35,1) 86%)",
+        }}
+      ></div> */}
       <div className="h-[100vh]"></div>
     </>
   );
--- components/common/landing-section.tsx
+++ components/common/landing-section.tsx
@@ -18,7 +18,7 @@ export const LandingSection = () => {
         start: "top top",
         end: "+=700",
         scrub: 0.5,
-        markers: true,
+        // markers: true,
       },
     });
     tl.to(
@@ -40,73 +40,75 @@ export const LandingSection = () => {
   }, []);
 
   return (
-    <div
-      ref={landingRef}
-      className={twMerge(
-        "w-full h-[100vh] min-h[100dvh] relative overflow-hidden"
-      )}
-    >
-      <div className="Image-section absolute  w-full h-full">
-        {" "}
-        <Image
-          src={`/landing/sky.png`}
-          alt={"image"}
-          fill
-          className="absolute object-cover"
-          quality={100}
-        />
-      </div>
+    <>
       <div
-        ref={mountain1Ref}
-        className="Image-section z-[1] absolute transform translate-y-[20%] w-full h-full"
+        ref={landingRef}
+        className={twMerge(
+          "w-full h-[100vh] min-h[100dvh] relative overflow-hidden bg-gradient-to-b from-transparent to-black"
+        )}
       >
-        {" "}
-        <Image
-          src={`/landing/mountain3.png`}
-          alt={"image"}
-          fill
-          className="absolute object-cover"
-          quality={100}
-        />
-      </div>
-      <div
-        ref={mountain2Ref}
-        className="Image-section  absolute left-0 transform translate-y-[30%]  z-[2]  w-full h-full"
-      >
-        <div className="relative w-full h-full transform lg:translate-x-[-10%]">
+        <div className="Image-section absolute  w-full h-full">
           {" "}
           <Image
-            src={`/landing/mountain2.png`}
+            src={`/landing/sky.png`}
             alt={"image"}
             fill
-            className="absolute object-cover md:object-contain"
+            className="absolute object-cover"
             quality={100}
           />
         </div>
-      </div>
-
-      <div
-        ref={mountain3Ref}
-        className="Image-section  absolute right-0 transform translate-y-[50%] z-[5]  w-full h-full"
-      >
-        {" "}
-        <Image
-          src={`/landing/mountain1.png`}
-          alt={"image"}
-          fill
-          className="absolute object-cover z-[4]"
-          quality={100}
-        />
-        <div className="absolute w-full md:w-[50%] h-[50vh] md:h-[50vh] transform translate-y-[35%] md:translate-y-[50%]  translate-x-[-10%] ">
+        <div
+          ref={mountain1Ref}
+          className="Image-section z-[1] absolute transform translate-y-[20%] w-full h-full"
+        >
+          {" "}
           <Image
-            src={`/landing/person.png`}
+            src={`/landing/mountain3.png`}
             alt={"image"}
             fill
-            className="absolute bottom-0 object-contain"
+            className="absolute object-cover"
             quality={100}
           />
         </div>
+        <div
+          ref={mountain2Ref}
+          className="Image-section  absolute left-0 transform translate-y-[30%]  z-[2]  w-full h-full"
+        >
+          <div className="relative w-full h-full transform lg:translate-x-[-10%]">
+            {" "}
+            <Image
+              src={`/landing/mountain2.png`}
+              alt={"image"}
+              fill
+              className="absolute object-cover md:object-contain"
+              quality={100}
+            />
+          </div>
+        </div>
+
+        <div
+          ref={mountain3Ref}
+          className="Image-section opacity-1 absolute right-0 transform translate-y-[50%] z-[5]  w-full h-full"
+        >
+          {" "}
+          <Image
+            src={`/landing/mountain1.png`}
+            alt={"image"}
+            fill
+            className="absolute object-cover z-[4] bg-gradient-to-b from-transparent to-black"
+            quality={100}
+          />
+          <div className="absolute w-full md:w-[50%] h-[50vh] md:h-[50vh] transform translate-y-[35%] md:translate-y-[50%]  translate-x-[-10%] ">
+            <Image
+              src={`/landing/person.png`}
+              alt={"image"}
+              fill
+              className="absolute bottom-0 object-contain"
+              quality={100}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };

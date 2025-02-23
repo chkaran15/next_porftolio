fecdfa4f0d49f3250a08aa42a347284e24e61a5b
Karan Chaudhary <tharukaran930@gmail.com>
2025-02-23 15:55:29 +0545

    chore: some changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function About() {
   return (
     <>
-      {/* <TransistionAnimation title={"about"} /> */}
+      <TransistionAnimation title={"about"} />
       <LandingSection />
       <div className="h-[100vh]"></div>
     </>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  exports: "output",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

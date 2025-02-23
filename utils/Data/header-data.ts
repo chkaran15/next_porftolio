3130251462871d44acb5608923a2f5476d4a70c5
Karan Chaudhary <tharukaran930@gmail.com>
2025-02-23 16:01:33 +0545

    chore: fixes

--- utils/Data/header-data.ts
+++ utils/Data/header-data.ts
@@ -1,18 +1,18 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  {
-    total_number: "50+",
-    title: "Projets Done",
-    image: "./header/videos.png",
-  },
-  {
-    total_number: "10+",
-    title: "Online Courses",
-    image: "./header/Ring.png",
-  },
-  {
-    total_number: "10+",
-    title: "Language Learned",
-    image: "./header/tutor.png",
-  },
-];
+    {
+        total_number:"50+",
+        title:"Projets Done",
+        image:"/header/videos.png"
+    },
+    {
+        total_number:"10+",
+        title:"Online Courses",
+        image:"/header/Ring.png"
+    },
+    {
+        total_number:"10+",
+        title:"Language Learned",
+        image:"/header/tutor.png"
+    }
+]
--- utils/Data/program-data.ts
+++ utils/Data/program-data.ts
@@ -1,54 +1,51 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
-  {
-    id: 1,
-    title: "Figma UI UX Design..",
-    url: "./programs/Image.png",
-    description:
-      "Use Figma to get a job in UI Design, User Interface, User Experience design.",
-    rating: {
-      rate: "4.3",
-      reviews: 16325,
+    {
+        id:1,
+        title:"Figma UI UX Design..",
+        url:"/programs/Image.png",
+        description:"Use Figma to get a job in UI Design, User Interface, User Experience design.",
+        rating:{
+            rate:"4.3",
+            reviews:16325,
+        },
+        user:{
+            name:"Jane Cooper",
+            enrolled:"2001 Enrolled",
+            url:"/programs/Avatar.png"
+        },
+        price:"17.84"
     },
-    user: {
-      name: "Jane Cooper",
-      enrolled: "2001 Enrolled",
-      url: "./programs/Avatar.png",
+    {
+        id:2,
+        title:"Learn With Shoaib",
+        url:"/programs/Image1.png",
+        description:"Design Web Sites and Mobile Apps that Your Users Love and Return to Again.",
+        rating:{
+            rate:"3.9",
+            reviews:8332,
+        },
+        user:{
+            name:"Jenny Wilson",
+            enrolled:"2001 Enrolled",
+            url:"/programs/Avatar1.png"
+        },
+        price:"8.99"
     },
-    price: "17.84",
-  },
-  {
-    id: 2,
-    title: "Learn With Shoaib",
-    url: "./programs/Image1.png",
-    description:
-      "Design Web Sites and Mobile Apps that Your Users Love and Return to Again.",
-    rating: {
-      rate: "3.9",
-      reviews: 8332,
-    },
-    user: {
-      name: "Jenny Wilson",
-      enrolled: "2001 Enrolled",
-      url: "./programs/Avatar1.png",
-    },
-    price: "8.99",
-  },
-  {
-    id: 3,
-    title: "Building User Interface",
-    url: "./programs/Image2.png",
-    description:
-      "Learn how to apply User Experience (UX) principles to your website designs.",
-    rating: {
-      rate: "4.2",
-      reviews: 125,
-    },
-    user: {
-      name: "Esther Howard",
-      enrolled: "2001 Enrolled",
-      url: "./programs/Avatar2.png",
-    },
-    price: "11.70",
-  },
-];
+    {
+        id:3,
+        title:"Building User Interface",
+        url:"/programs/Image2.png",
+        description:"Learn how to apply User Experience (UX) principles to your website designs.",
+        rating:{
+            rate:"4.2",
+            reviews:125,
+        },
+        user:{
+            name:"Esther Howard",
+            enrolled:"2001 Enrolled",
+            url:"/programs/Avatar2.png"
+        },
+        price:"11.70"
+    }
+]
